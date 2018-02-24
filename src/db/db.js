dcda4b0f1b23869ce31dbb9b08e8997631b041b9
Quang Huy <myheaven291@gmail.com>
2018-02-24 12:52:38 +0700

    create db for setting

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,9 @@
 import Datastore from 'react-native-local-mongodb';
 
-console.log(Datastore);
-var db = new Datastore({fileName: 'asyncStorageKey', autoLoad: true});
+var db = new Datastore({ fileName: 'asyncStorageKey', autoLoad: true });
 
-export function createSetting(objSetting) {
-    db.insert(objSetting, (err, res) => {
+export function createSetting(setting) {
+    db.insert(setting, (err, res) => {
         if (err) {
             console.log(`Can't not save setting: `, err)
         } else {
@@ -14,11 +13,29 @@ export function createSetting(objSetting) {
 }
 
 export function getSetting() {
-    db.find({id: 'setting'}, (err, res) => {
+    db.find({ id: 'setting' }, (err, res) => {
         if (err) {
             console.log('Cant get setting')
         } else {
             console.log(res)
+        }
+    })
+}
+
+
+export function saveSetting() {
+    let newSetting = {
+        isUpper: true,
+        textColor: 'black',
+        numsWord: 5,
+        numsNewWord: 1,
+        notification: true
+    }
+    db.update({ id: 'setting' }, { $set: newSetting }, (err, res) => {
+        if (err) {
+            console.log('Cant save setting: ', err)
+        } else {
+            console.log('Save setting success')
         }
     })
 }
--- src/screens/setting.js
+++ src/screens/setting.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { TabNavigator } from 'react-navigation';
+
+import * as db from '../db/db';
 
 export default class Setting extends React.PureComponent {
   render() {
     return (
       <View style={styles.container}>
+        <TouchableOpacity onPress={() => db.saveSetting()}>
+          <Text>Save</Text>
+        </TouchableOpacity>
         <Text>Setting!</Text>
       </View>
     );
